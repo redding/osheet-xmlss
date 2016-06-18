562448196fd032d250d63457a5308ebbbcbda643
Collin Redding <TempestTTU@gmail.com>
2016-06-17 22:22:38 -0500

    Hotfix: Update to work with newer versions of ruby
    This updates the gem to work with newer versions of ruby. This
    didn't require any real changes to the code. The only changes
    needed were adding the gem dir to the load path for the
    bench/examples scripts and adding the `choice` backfill for ruby
    1.8.7. We aren't currently using `choice` in the test suite but
    this updates to match how GGem generates the `test/helper`.

--- bench/bench.rb
+++ bench/bench.rb
@@ -1,4 +1,6 @@
 # $ bundle exec ruby bench/bench.rb
 
+$LOAD_PATH.unshift(File.expand_path("../..", __FILE__))
 require 'bench/bench_runner'
+
 OsheetXmlssBenchRunner.new
--- examples/examples.rb
+++ examples/examples.rb
@@ -1,5 +1,7 @@
 # $ bundle exec ruby examples/examples.rb
 
+$LOAD_PATH.unshift(File.expand_path("../..", __FILE__))
+
 require 'examples/trivial'
 require 'examples/basic'
 require 'examples/basic_with_templates'
